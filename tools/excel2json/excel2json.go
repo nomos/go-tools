a03cc89e7a926394f7e9087f23611e02d9661620
DESKTOP-N8VJUR3\wqs <1201013@qq.com>
2021-05-31 00:54:26 +0800

    fix

--- tools/excel2json/excel2json.go
+++ tools/excel2json/excel2json.go
@@ -3,7 +3,7 @@ package excel2json
 import (
 	"encoding/json"
 	"errors"
-	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
 	"github.com/nomos/go-log/log"
 	"github.com/nomos/go-lokas/util"
 	"github.com/nomos/go-lokas/util/slice"
