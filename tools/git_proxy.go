91f30ebfe6e69891e9ba0f63d9291c36ad448aea
wqs <1201013@qq.com>
2021-08-20 12:01:45 +0800

    update

---
+++ tools/git_proxy.go
@@ -0,0 +1,97 @@
+package tools
+
+import (
+	"fmt"
+	"github.com/nomos/go-tools/ui"
+	"github.com/ying32/govcl/vcl"
+	"github.com/ying32/govcl/vcl/types"
+)
+
+var _ ui.IFrame = (*GitProxyTool)(nil)
+
+type GitProxyTool struct {
+	*vcl.TFrame
+	ui.ConfigAble
+	GitProxyEdit *ui.EditLabel
+
+}
+
+func NewGitProxyTool(owner vcl.IWinControl,option... ui.FrameOption) (root *GitProxyTool)  {
+	vcl.CreateResFrame(owner, &root)
+	for _,o:=range option {
+		o(root)
+	}
+	return
+}
+
+func (this *GitProxyTool) setup(){
+	this.SetAlign(types.AlClient)
+	this.SetConfig(this.Config().Sub("git"))
+	line:=ui.CreateLine(types.AlTop,44,this)
+	toggle:=ui.NewToggleFrame(line,false)
+	toggle.SetParent(line)
+	toggle.SetAlign(types.AlLeft)
+	toggle.OnCreate()
+	ui.CreateSeg(10,line)
+	this.GitProxyEdit=ui.NewEditLabel(line,"git proxy",140,ui.EDIT_TYPE_STRING)
+	this.GitProxyEdit.SetParent(line)
+	this.GitProxyEdit.SetChangeOnEdit(true)
+	this.GitProxyEdit.SetAlign(types.AlLeft)
+	toggle.BorderSpacing().SetTop(12)
+	toggle.OnChecked = func(b bool) {
+		if b {
+			this.Config().Set("git_proxy_on",true)
+			this.gitProxyOn()
+		} else {
+			this.Config().Set("git_proxy_on",false)
+			this.gitProxyOff()
+		}
+	}
+	this.GitProxyEdit.OnValueChange = func(label *ui.EditLabel, editType ui.EDIT_TYPE, value interface{}) {
+		this.Config().Set("git_proxy",this.GitProxyEdit.String())
+	}
+	this.GitProxyEdit.OnCreate()
+	if this.Config().GetBool("git_proxy_on") {
+		toggle.Check()
+	} else {
+		toggle.Uncheck()
+	}
+}
+
+func (this *GitProxyTool) gitProxyOn(){
+	if this.Config().GetString("git_proxy") == "" {
+		this.GitProxyEdit.SetFocus()
+		return
+	}
+	this.GetLogger().(*ui.ConsoleShell).ExecShellCmd(
+		fmt.Sprintf("git config --global http.proxy %s;git config --global https.proxy %s",
+			this.Config().GetString("git_proxy"),
+			this.Config().GetString("git_proxy"),
+		),false).Await()
+}
+
+func (this *GitProxyTool) gitProxyOff(){
+	this.GetLogger().(*ui.ConsoleShell).ExecShellCmd("git config --global --unset http.proxy;git config --global  --unset https.proxy",false).Await()
+}
+
+func (this *GitProxyTool) OnCreate(){
+	this.setup()
+}
+
+func (this *GitProxyTool) OnDestroy(){
+
+}
+
+func (this *GitProxyTool) OnEnter(){
+
+}
+
+func (this *GitProxyTool) OnExit(){
+
+}
+
+func (this *GitProxyTool) Clear(){
+
+}
+
+
--- tools/pics/img_tool/dds_convertor.go
+++ tools/pics/img_tool/dds_convertor.go
@@ -1,9 +1,16 @@
 package img_tool
 
 import (
+	"github.com/nomos/go-lokas/log"
+	"github.com/nomos/go-lokas/util"
+	"github.com/nomos/go-tools/tools/pics/img_dds"
 	"github.com/nomos/go-tools/ui"
 	"github.com/ying32/govcl/vcl"
 	"github.com/ying32/govcl/vcl/types"
+	"image"
+	"image/png"
+	"os"
+	"strings"
 )
 
 var _ ui.IFrame = (*DDSConverter)(nil)
@@ -35,19 +42,57 @@ func (this *DDSConverter) setup(){
 	tag.OnCreate()
 	tag.SetParent(line2)
 	dirBtn.SetOpenDirDialog("DDS目录")
+	if this.Config().GetString("dds_folder")!="" {
+		dirBtn.SetPath(this.Config().GetString("dds_folder"))
+	}
+	tag.SetBool(this.Config().GetBool("bgr_flip"))
 	dirBtn.OnEdit = func(s string) {
-		
+		if util.IsFileExist(s) {
+			this.Config().Set("dds_folder",s)
+		}
 	}
 	dirBtn.OnOpen = func(s string) {
-
+		if util.IsFileExist(s) {
+			this.Config().Set("dds_folder",s)
+		}
 	}
 	transBtn.SetOnClick(func(sender vcl.IObject) {
-
+		this.convertDDSFolder(this.Config().GetString("dds_folder"),this.Config().GetBool("bgr_flip"))
 	})
 	tag.OnValueChange = func(label *ui.EditLabel, editType ui.EDIT_TYPE, value interface{}) {
-		
+		this.Config().Set("bgr_flip",tag.Bool())
 	}
+}
 
+func (this *DDSConverter) convertDDSFolder(filePath string,flip bool)error{
+	files,err:=util.WalkDirFiles(filePath,true)
+	if err != nil {
+		return err
+	}
+	file1:=util.FilterFileWithExt(files,".dds")
+	for _,file:=range file1 {
+		this.convertDDS(file,flip)
+	}
+	return nil
+}
+
+func (this *DDSConverter) convertDDS(filePath string,flip bool){
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Panicf(err)
+	}
+	defer file.Close()
+	tex, err := img_dds.Decode(file,flip)
+	if err != nil {
+		log.Errorf("decode errror", err)
+		return
+	}
+	this.GetLogger().Infof("decoded",tex.Width, tex.Height, len(tex.Data),filePath)
+	image := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{tex.Width, tex.Height}})
+	image.Pix = tex.Data
+	file1:=strings.Replace(filePath,".dds",".png",1)
+	out, _ := os.Create(file1)
+	png.Encode(out, image)
 }
 
 func (this *DDSConverter) OnCreate(){
