3aea42d789098961b716c5f158e9d8776a98f97f
wqs <1201013@qq.com>
2021-09-15 16:15:52 +0800

    fix drop file

--- tools/pics/img_tool/img2arraybuffer.go
+++ tools/pics/img_tool/img2arraybuffer.go
@@ -3,18 +3,21 @@ package img_tool
 import (
     "github.com/atotto/clipboard"
     "github.com/nomos/go-lokas/log"
+    "github.com/nomos/go-lokas/promise"
     "github.com/nomos/go-tools/tools/pics/img_png"
     "github.com/nomos/go-tools/ui"
     "github.com/ying32/govcl/vcl"
     "github.com/ying32/govcl/vcl/types"
     "path"
     "strings"
+    "time"
 )
 
 type TImage2ArrayBuffer struct {
     *vcl.TFrame
     DropDownPanel     *vcl.TPanel
     Label1            *vcl.TLabel
+    dropFile string
     entered bool
     ui.ConfigAble
 }
@@ -54,7 +57,31 @@ func (this *TImage2ArrayBuffer) setup(){
 func (this *TImage2ArrayBuffer) OnCreate(){
     this.setup()
     this.DropDownPanel.SetOnMouseEnter(func(sender vcl.IObject) {
+        log.Info("enter drop down")
         this.entered = true
+        filePath:=this.dropFile
+        if filePath=="" {
+            return
+        }
+
+        w,h,data,err:=img_png.Png2CompressedBase64(filePath)
+        if err != nil {
+            this.GetLogger().Error(err.Error())
+            return
+        }
+        this.GetLogger().Infof("width:",w)
+        this.GetLogger().Infof("height:",h)
+        this.GetLogger().Infof("data:",data)
+        fileName:=strings.Replace(path.Base(filePath),".png","",-1)
+        clipboard.WriteAll(fileName+" : `"+data+"`,")
+        this.GetLogger().Info("已拷贝到剪切板")
+        //if path.Ext(filePath) == ".png" {
+        //    outPath:= strings.Replace(filePath,".png",".txt",1)
+        //    err:=ioutil.WriteFile(outPath,[]byte(strconv.Itoa(w)+" "+strconv.Itoa(h)+"\n"+data),0666)
+        //    if err != nil {
+        //        this.GetLogger().Error(err.Error())
+        //    }
+        //}
     })
     this.DropDownPanel.SetOnMouseLeave(func(sender vcl.IObject) {
         this.entered = false
@@ -62,30 +89,13 @@ func (this *TImage2ArrayBuffer) OnCreate(){
 
     this.GetListener().On("DropFile", func(i ...interface{}) {
         log.Warnf("listener")
-        if this.entered {
-            paths:=i[0].([]string)
-            this.GetLogger().Warnf("DropFile",paths)
-            filePath:=paths[0]
-
-            w,h,data,err:=img_png.Png2CompressedBase64(filePath)
-            if err != nil {
-                this.GetLogger().Error(err.Error())
-                return
-            }
-            this.GetLogger().Infof("width:",w)
-            this.GetLogger().Infof("height:",h)
-            this.GetLogger().Infof("data:",data)
-            fileName:=strings.Replace(path.Base(filePath),".png","",-1)
-            clipboard.WriteAll(fileName+" : `"+data+"`,")
-            this.GetLogger().Info("已拷贝到剪切板")
-            //if path.Ext(filePath) == ".png" {
-            //    outPath:= strings.Replace(filePath,".png",".txt",1)
-            //    err:=ioutil.WriteFile(outPath,[]byte(strconv.Itoa(w)+" "+strconv.Itoa(h)+"\n"+data),0666)
-            //    if err != nil {
-            //        this.GetLogger().Error(err.Error())
-            //    }
-            //}
-        }
+        time.Sleep(1000)
+        paths:=i[0].([]string)
+        this.GetLogger().Warnf("DropFile",paths)
+        this.dropFile = paths[0]
+        promise.SetTimeout(time.Second*1, func(timeout *promise.Timeout) {
+            this.dropFile = ""
+        })
     })
 }
 
