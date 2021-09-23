c123dd9398ba2047ba713285f0351cda01248c75
wqs <1201013@qq.com>
2021-09-23 18:59:36 +0800

    update

--- erpc/app.go
+++ erpc/app.go
@@ -1,17 +1,24 @@
 package erpc
 
 import (
+	"github.com/nomos/go-lokas"
 	"github.com/nomos/go-lokas/lox"
 	"github.com/nomos/go-lokas/network"
+	"github.com/nomos/go-lokas/protocol"
 )
 
 //一个和electron typescript通信来实现访问golang原生功能的框架
+func WithPort(port string)Option{
+	return func(app *App) {
+		app.Port = port
+	}
+}
 
 type Option func(app *App)
 
 type App struct {
 	*lox.Gate
-	Port int
+	Port string
 }
 
 func NewApp(opts ...Option) *App {
@@ -21,13 +28,29 @@ func NewApp(opts ...Option) *App {
 			ISessionManager: network.NewDefaultSessionManager(true),
 		},
 	}
-	ret.SetType("Gate")
 	for _, o := range opts {
 		o(ret)
 	}
+	ret.Gate.LoadCustom("0.0.0.0",ret.Port,protocol.BINARY,lox.Websocket)
 	return ret
 }
 
-func (this *App) Start() {
+func (this *App) SessionCreator(conn lokas.IConn) lokas.ISession {
+	sess := lox.NewPassiveSession(conn, this.GetProcess().GenId(), this)
+	sess.AuthFunc = func(data []byte) error {
+		return nil
+	}
+	sess.Protocol = this.Protocol
+	this.ISessionManager.AddSession(sess.GetId(), sess)
+	this.GetProcess().AddActor(sess)
+	this.GetProcess().StartActor(sess)
+	return sess
+}
 
+func (this *App) Start() error{
+	return this.Gate.Start()
+}
+
+func (this *App) Stop() error{
+	return this.Gate.Stop()
 }
