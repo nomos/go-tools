802c67a9ecf70bf60e1539f4ada9d6d397ee1453
wqs <1201013@qq.com>
2022-05-22 15:23:19 +0800

    fix

--- ui/pjson/json_schema.go
+++ ui/pjson/json_schema.go
@@ -28,32 +28,31 @@ type Schema struct {
 	value      string
 	parent     *Schema
 	children   []ui.ITreeSchema
-	collapse bool
+	collapse   bool
 
-	node *vcl.TTreeNode
-	tree ui.ITree
+	node      *vcl.TTreeNode
+	tree      ui.ITree
 	collapsed bool
-
 }
 
-func (this *Schema) SetNode(node *vcl.TTreeNode){
+func (this *Schema) SetNode(node *vcl.TTreeNode) {
 	this.node = node
 }
 
-func (this *Schema) Node()*vcl.TTreeNode{
+func (this *Schema) Node() *vcl.TTreeNode {
 	return this.node
 }
 
-func (this *Schema) SetExpanded(){
+func (this *Schema) SetExpanded() {
 	this.collapse = false
 }
 
-func (this *Schema) SetCollapsed(){
+func (this *Schema) SetCollapsed() {
 	this.collapse = true
 }
 
-func (this *Schema) UpdateNode()bool{
-	if this.node!=nil {
+func (this *Schema) UpdateNode() bool {
+	if this.node != nil {
 		go func() {
 			this.node.SetImageIndex(icons.GetImageList(16, 16).GetImageIndex(this.Image()))
 			this.node.SetSelectedIndex(icons.GetImageList(16, 16).GetImageIndex(this.Image()))
@@ -61,10 +60,10 @@ func (this *Schema) UpdateNode()bool{
 			vcl.ThreadSync(func() {
 				defer func() {
 					if r := recover(); r != nil {
-						util.Recover(r,false)
+						util.Recover(r, false)
 					}
 				}()
-				if this.node!=nil {
+				if this.node != nil {
 					this.node.SetText(this.String())
 				}
 			})
@@ -80,7 +79,7 @@ func (this *Schema) Collapse() bool {
 }
 
 func (this *Schema) SetCollapse(b bool) {
-	if this.collapse!= b {
+	if this.collapse != b {
 		this.tree.UpdateTree(this.Root())
 	}
 }
@@ -105,39 +104,39 @@ func (this *Schema) SetValue(s string) {
 	this.value = s
 }
 
-func (this *Schema) Key()string {
+func (this *Schema) Key() string {
 	return this.key
 }
 
-func (this *Schema) Value()string {
+func (this *Schema) Value() string {
 	return this.value
 }
 
-func (this *Schema) Idx()int{
+func (this *Schema) Idx() int {
 	return this.index
 }
 
-func (this *Schema) SetIdx(id int){
+func (this *Schema) SetIdx(id int) {
 	this.index = id
 }
 
-func (this *Schema) InnerIdx()int{
+func (this *Schema) InnerIdx() int {
 	return this.innerIndex
 }
 
-func (this *Schema) SetInnerIdx(id int){
+func (this *Schema) SetInnerIdx(id int) {
 	this.innerIndex = id
 }
 
-func (this *Schema) Parent()ui.ITreeSchema {
+func (this *Schema) Parent() ui.ITreeSchema {
 	return this.parent
 }
 
-func (this *Schema) Children()[]ui.ITreeSchema {
+func (this *Schema) Children() []ui.ITreeSchema {
 	return this.children
 }
 
-func (this *Schema) Image()string{
+func (this *Schema) Image() string {
 	switch this.Type {
 	case Null:
 		return "null_icon"
@@ -160,7 +159,7 @@ func (this *Schema) typeof(d interface{}) Type {
 		return Null
 	}
 	switch reflect.TypeOf(d).Kind().String() {
-	case "ptr","struct":
+	case "ptr", "struct":
 		return Object
 	case "map":
 		return Object
@@ -176,32 +175,32 @@ func (this *Schema) typeof(d interface{}) Type {
 	return Null
 }
 
-func (this *Schema) Unmarshal(key string,index int, d interface{}) (*Schema, error) {
+func (this *Schema) Unmarshal(key string, index int, d interface{}) (*Schema, error) {
 	t := this.typeof(d)
 	if this.parent == nil && t != Object && t != Array {
 		return nil, errors.New("parent cant be " + t.String())
 	}
-	if key!="" {
+	if key != "" {
 		this.key = key
 	}
-	if index>=0 {
+	if index >= 0 {
 		this.index = index
 	}
 	this.Type = t
 	switch t {
 	case Object:
-		if d1,ok:=d.(orderedmap.OrderedMap);ok {
-			for _,k:=range d1.Keys() {
-				schema:=this.AppendChild()
-				v,ok:=d1.Get(k)
+		if d1, ok := d.(orderedmap.OrderedMap); ok {
+			for _, k := range d1.Keys() {
+				schema := this.AppendChild()
+				v, ok := d1.Get(k)
 				if ok {
-					schema.Unmarshal(k,-1,v)
+					schema.Unmarshal(k, -1, v)
 				}
 			}
-		} else if d1,ok:=d.(map[string]interface{});ok {
-			for k,v:=range d1 {
-				schema:=this.AppendChild()
-				schema.Unmarshal(k,-1,v)
+		} else if d1, ok := d.(map[string]interface{}); ok {
+			for k, v := range d1 {
+				schema := this.AppendChild()
+				schema.Unmarshal(k, -1, v)
 			}
 		}
 
@@ -209,8 +208,8 @@ func (this *Schema) Unmarshal(key string,index int, d interface{}) (*Schema, err
 	case Array:
 		d1 := d.([]interface{})
 		for k, v := range d1 {
-			schema:=this.AppendChild()
-			schema.Unmarshal("",k,v)
+			schema := this.AppendChild()
+			schema.Unmarshal("", k, v)
 		}
 		break
 	case String:
@@ -221,7 +220,7 @@ func (this *Schema) Unmarshal(key string,index int, d interface{}) (*Schema, err
 			this.value = "NaN"
 			break
 		}
-		this.value = strconv.FormatFloat(d.(float64),'f', -1, 64)
+		this.value = strconv.FormatFloat(d.(float64), 'f', -1, 64)
 	case Boolean:
 		if d.(bool) {
 			this.value = "true"
@@ -233,100 +232,100 @@ func (this *Schema) Unmarshal(key string,index int, d interface{}) (*Schema, err
 		this.value = "null"
 		break
 	}
-	return this,nil
+	return this, nil
 }
 
-func (this *Schema) AddChild(s ui.ITreeSchema)ui.ITreeSchema {
-	child:=s.(*Schema)
+func (this *Schema) AddChild(s ui.ITreeSchema) ui.ITreeSchema {
+	child := s.(*Schema)
 	child.parent = this
 	this.children = append(this.children, s)
-	child.innerIndex = len(this.children)-1
+	child.innerIndex = len(this.children) - 1
 	return s
 }
 
-func (this *Schema) AppendChild()*Schema {
-	s:=NewSchema()
+func (this *Schema) AppendChild() *Schema {
+	s := NewSchema()
 	s.parent = this
 	this.children = append(this.children, s)
-	s.innerIndex = len(this.children)-1
+	s.innerIndex = len(this.children) - 1
 	return s
 }
 
-func (this *Schema) Root()ui.ITreeSchema {
-	root:=this
+func (this *Schema) Root() ui.ITreeSchema {
+	root := this
 	for {
-		if root.Parent()==nil {
+		if root.Parent() == nil {
 			return root
 		}
 		root = root.parent
 	}
 }
 
-func (this *Schema) DetachFromParent()*Schema {
+func (this *Schema) DetachFromParent() *Schema {
 	this.parent.Detach(this)
 	return this
 }
 
-func (this *Schema) GetRootTree()[]int {
-	ret:=make([]int,0)
-	root:=this
+func (this *Schema) GetRootTree() []int {
+	ret := make([]int, 0)
+	root := this
 	for {
-		if root.Parent()==nil {
+		if root.Parent() == nil {
 			break
 		}
-		if root.innerIndex== -1 {
-			return make([]int,0)
+		if root.innerIndex == -1 {
+			return make([]int, 0)
 		}
 		ret = append(ret, root.innerIndex)
 		root = root.parent
 	}
-	ret1:=make([]int,0)
-	for i:=len(ret)-1;i>=0;i-- {
+	ret1 := make([]int, 0)
+	for i := len(ret) - 1; i >= 0; i-- {
 		ret1 = append(ret1, ret[i])
 	}
 	return ret1
 }
 
-func (this *Schema) String()string {
-	ret:=""
+func (this *Schema) String() string {
+	ret := ""
 	if this.index != -1 {
-		ret+="[Item"+strconv.Itoa(this.index)+"]:   "
+		ret += "[Item" + strconv.Itoa(this.index) + "]:   "
 	}
-	if this.key !="" {
-		ret+=`"`+this.key +`":`
+	if this.key != "" {
+		ret += `"` + this.key + `":`
 	}
-	ret = stringutil.AddStringGap(ret,10,6)
+	ret = stringutil.AddStringGap(ret, 10, 6)
 
 	switch this.Type {
 	case Object:
-		return ret+"object"
+		return ret + "object"
 	case Array:
-		return ret+"array"
+		return ret + "array"
 	case String:
-		return ret+`"`+this.value +`"`
+		return ret + `"` + this.value + `"`
 	default:
-		return ret+this.value
+		return ret + this.value
 	}
 }
 
-func (this *Schema) ToObj()interface{} {
+func (this *Schema) ToObj() interface{} {
 	switch this.Type {
 	case Object:
-		ret:=orderedmap.New()
-		for _,container:=range this.children {
-			ret.Set(container.Key(),container.ToObj())
+		ret := orderedmap.New()
+		for _, container := range this.children {
+			ret.Set(container.Key(), container.ToObj())
 		}
 		return ret
 	case Array:
-		ret:=make([]interface{},0)
-		for _,container:=range this.children {
+		ret := make([]interface{}, 0)
+		for _, container := range this.children {
 			ret = append(ret, container.ToObj())
 		}
 		return ret
 	case String:
 		return this.value
 	case Number:
-		ret,err:= strconv.ParseFloat(this.value,64)
+		ret, err := strconv.ParseFloat(this.value, 64)
 		if err != nil {
 			log.Error(err.Error())
 			return nil
@@ -344,14 +343,14 @@ func (this *Schema) ToObj()interface{} {
 	return nil
 }
 
-func (this *Schema) ToKeyString()string {
+func (this *Schema) ToKeyString() string {
 	if this.index != -1 {
-		return "["+strconv.Itoa(this.index)+"]"
+		return "[" + strconv.Itoa(this.index) + "]"
 	}
 	return this.key
 }
 
-func (this *Schema) ToValueString()string {
+func (this *Schema) ToValueString() string {
 	switch this.Type {
 	case Object:
 		return "[object]"
@@ -362,12 +361,12 @@ func (this *Schema) ToValueString()string {
 	}
 }
 
-func (this *Schema) Clone()ui.ITreeSchema {
+func (this *Schema) Clone() ui.ITreeSchema {
 	return this.clone(true)
 }
 
-func (this *Schema) clone(root bool)*Schema {
-	schema:=NewSchema()
+func (this *Schema) clone(root bool) *Schema {
+	schema := NewSchema()
 	schema.Type = this.Type
 	schema.value = this.value
 	schema.key = this.key
@@ -379,29 +378,29 @@ func (this *Schema) clone(root bool)*Schema {
 		schema.index = -1
 		schema.innerIndex = -1
 	}
-	for _,s:=range this.children {
-		s1:=s.(*Schema).clone(false)
+	for _, s := range this.children {
+		s1 := s.(*Schema).clone(false)
 		schema.AddChild(s1)
 	}
 	return schema
 }
 
-func (this *Schema) IsRoot()bool {
-	return this.parent== nil
+func (this *Schema) IsRoot() bool {
+	return this.parent == nil
 }
 
-func (this *Schema) IsObjectElem()bool {
-	return !this.IsRoot()&&this.index == -1
+func (this *Schema) IsObjectElem() bool {
+	return !this.IsRoot() && this.index == -1
 }
 
-func (this *Schema) IsArrayElem()bool {
-	return !this.IsRoot()&&this.index != -1
+func (this *Schema) IsArrayElem() bool {
+	return !this.IsRoot() && this.index != -1
 }
 
-func (this *Schema) ToString(format bool)string {
+func (this *Schema) ToString(format bool) string {
 	switch this.Type {
-	case Object,Array:
-		ret,err:=json.Marshal(this.ToObj())
+	case Object, Array:
+		ret, err := json.Marshal(this.ToObj())
 		if err != nil {
 			log.Error(err.Error())
 			return ""
@@ -418,22 +417,22 @@ func (this *Schema) ToString(format bool)string {
 	}
 }
 
-func (this *Schema) Detach(s ui.ITreeSchema)ui.ITreeSchema {
-	index:=-1
-	newContainer:=make([]ui.ITreeSchema,0)
-	for i,v:=range this.children {
-		if v== s {
+func (this *Schema) Detach(s ui.ITreeSchema) ui.ITreeSchema {
+	index := -1
+	newContainer := make([]ui.ITreeSchema, 0)
+	for i, v := range this.children {
+		if v == s {
 			index = i
 			continue
 		}
 		newContainer = append(newContainer, v)
-		v.SetInnerIdx(len(newContainer)-1)
+		v.SetInnerIdx(len(newContainer) - 1)
 		if this.Type == Array {
 			v.SetIdx(v.InnerIdx())
 		}
 	}
 	this.children = newContainer
-	if index!= -1 {
+	if index != -1 {
 		this.parent = nil
 		if this.index != -1 {
 			this.index = -1
@@ -443,60 +442,60 @@ func (this *Schema) Detach(s ui.ITreeSchema)ui.ITreeSchema {
 	return this
 }
 
-func (this *Schema) defaultKey(key string)string{
+func (this *Schema) defaultKey(key string) string {
 	if key == "" {
 		key = "default"
 	}
-	keys:=[]string{}
-	for _,c:=range this.children {
+	keys := []string{}
+	for _, c := range this.children {
 		keys = append(keys, c.Key())
 	}
-	dupNum:=1
-	r:=regexp.MustCompile(`\([0-9]+\)$`)
-	for slice.HasString(keys,key) {
-		if  r.MatchString(key){
-			key = r.ReplaceAllString(key,"("+strconv.Itoa(dupNum)+")")
+	dupNum := 1
+	r := regexp.MustCompile(`\([0-9]+\)$`)
+	for slice.Has(keys, key) {
+		if r.MatchString(key) {
+			key = r.ReplaceAllString(key, "("+strconv.Itoa(dupNum)+")")
 		} else {
-			key = key+"("+strconv.Itoa(dupNum)+")"
+			key = key + "(" + strconv.Itoa(dupNum) + ")"
 		}
 		dupNum++
 	}
 	return key
 }
 
-func (this *Schema) Insert(s ui.ITreeSchema)ui.ITreeSchema {
-	parent:=this
-	index:=0
+func (this *Schema) Insert(s ui.ITreeSchema) ui.ITreeSchema {
+	parent := this
+	index := 0
 	if this.Type.IsValue() {
 		parent = this.parent
-		index = this.innerIndex+1
+		index = this.innerIndex + 1
 	}
 	if parent.Type == Array {
 		s.SetKey("")
 	} else {
 		s.SetKey(parent.defaultKey(s.Key()))
 	}
-	parent.insert(index,s.(*Schema))
+	parent.insert(index, s.(*Schema))
 	return s
 }
 
-func (this *Schema) insert(pos int,s *Schema) {
-	newC := make([]ui.ITreeSchema,0)
-	for _,v:=range this.children[0:pos] {
+func (this *Schema) insert(pos int, s *Schema) {
+	newC := make([]ui.ITreeSchema, 0)
+	for _, v := range this.children[0:pos] {
 		newC = append(newC, v)
-		v.SetInnerIdx(len(newC)-1)
+		v.SetInnerIdx(len(newC) - 1)
 		if this.Type == Array {
 			v.SetIdx(v.InnerIdx())
 		}
 	}
-	newC = append(newC,s)
-	s.innerIndex = len(newC)-1
+	newC = append(newC, s)
+	s.innerIndex = len(newC) - 1
 	if this.Type == Array {
 		s.index = s.innerIndex
 	}
-	for _,v:=range this.children[pos:] {
+	for _, v := range this.children[pos:] {
 		newC = append(newC, v)
-		v.SetInnerIdx(len(newC)-1)
+		v.SetInnerIdx(len(newC) - 1)
 		if this.Type == Array {
 			v.SetIdx(v.InnerIdx())
 		}
@@ -505,28 +504,28 @@ func (this *Schema) insert(pos int,s *Schema) {
 	s.parent = this
 }
 
-func (this *Schema) ChangeType(t Type)bool {
+func (this *Schema) ChangeType(t Type) bool {
 	if this.Type == t {
 		return false
 	}
 	if this.Type == Object || this.Type == Array {
-		if len(this.children)>0 {
+		if len(this.children) > 0 {
 			return false
 		}
 	}
 	switch this.Type {
-	case Object,Array:
-		if len(this.children)>0 {
+	case Object, Array:
+		if len(this.children) > 0 {
 			return false
 		} else {
 			this.value = t.Default()
 		}
-	case Number,String,Boolean,Null:
-		if t==Object||t==Array {
+	case Number, String, Boolean, Null:
+		if t == Object || t == Array {
 			this.value = t.Default()
-		} else if s,ok:=t.CheckValue(this.value);!ok {
+		} else if s, ok := t.CheckValue(this.value); !ok {
 			this.value = t.Default()
-		}  else {
+		} else {
 			this.value = s
 		}
 	}
@@ -535,7 +534,7 @@ func (this *Schema) ChangeType(t Type)bool {
 }
 
 func (this *Schema) TrySetKey(s string) bool {
-	if this.IsRoot()||(this.parent!=nil&&this.parent.Type==Array) {
+	if this.IsRoot() || (this.parent != nil && this.parent.Type == Array) {
 		return false
 	}
 	this.key = s
@@ -544,10 +543,10 @@ func (this *Schema) TrySetKey(s string) bool {
 
 func (this *Schema) TrySetValue(s string) bool {
 	switch this.Type {
-	case Object,Array:
+	case Object, Array:
 		return false
 	default:
-		s,ok:=this.Type.CheckValue(s)
+		s, ok := this.Type.CheckValue(s)
 		if ok {
 			this.value = s
 			return true
@@ -556,8 +555,8 @@ func (this *Schema) TrySetValue(s string) bool {
 	return false
 }
 
-func (this *Schema) MoveUp()bool {
-	if this.parent==nil {
+func (this *Schema) MoveUp() bool {
+	if this.parent == nil {
 		return false
 	}
 	if this.innerIndex == 0 {
@@ -567,27 +566,27 @@ func (this *Schema) MoveUp()bool {
 	return true
 }
 
-func (this *Schema) moveUp(s *Schema){
+func (this *Schema) moveUp(s *Schema) {
 	innerIndex := s.innerIndex
-	newContainer:=make([]ui.ITreeSchema,0)
-	for _,v:=range this.children[0:innerIndex-1] {
+	newContainer := make([]ui.ITreeSchema, 0)
+	for _, v := range this.children[0 : innerIndex-1] {
 		newContainer = append(newContainer, v)
-		v.SetInnerIdx(len(newContainer) -1)
+		v.SetInnerIdx(len(newContainer) - 1)
 		if this.Type == Array {
 			v.SetIdx(v.InnerIdx())
 		}
 	}
-	newContainer = append(newContainer,s)
-	s.innerIndex = len(newContainer) -1
+	newContainer = append(newContainer, s)
+	s.innerIndex = len(newContainer) - 1
 	if this.Type == Array {
 		s.index = s.innerIndex
 	}
-	for _,v:=range this.children[innerIndex-1:] {
+	for _, v := range this.children[innerIndex-1:] {
 		if v == s {
 			continue
 		}
 		newContainer = append(newContainer, v)
-		v.SetInnerIdx(len(newContainer) -1)
+		v.SetInnerIdx(len(newContainer) - 1)
 		if this.Type == Array {
 			v.SetIdx(v.InnerIdx())
 		}
@@ -595,8 +594,8 @@ func (this *Schema) moveUp(s *Schema){
 	this.children = newContainer
 }
 
-func (this *Schema) MoveDown()bool {
-	if this.parent==nil {
+func (this *Schema) MoveDown() bool {
+	if this.parent == nil {
 		return false
 	}
 	if this.innerIndex == len(this.parent.children)-1 {
@@ -606,27 +605,27 @@ func (this *Schema) MoveDown()bool {
 	return true
 }
 
-func (this *Schema) moveDown(s *Schema){
+func (this *Schema) moveDown(s *Schema) {
 	innerIndex := s.innerIndex
-	newContainer:=make([]ui.ITreeSchema,0)
-	for _,v:=range this.children[0:innerIndex+2] {
+	newContainer := make([]ui.ITreeSchema, 0)
+	for _, v := range this.children[0 : innerIndex+2] {
 		if v == s {
 			continue
 		}
 		newContainer = append(newContainer, v)
-		v.SetInnerIdx(len(newContainer) -1)
+		v.SetInnerIdx(len(newContainer) - 1)
 		if this.Type == Array {
 			v.SetIdx(v.InnerIdx())
 		}
 	}
-	newContainer = append(newContainer,s)
-	s.innerIndex = len(newContainer) -1
+	newContainer = append(newContainer, s)
+	s.innerIndex = len(newContainer) - 1
 	if this.Type == Array {
 		s.index = s.innerIndex
 	}
-	for _,v:=range this.children[innerIndex+2:] {
+	for _, v := range this.children[innerIndex+2:] {
 		newContainer = append(newContainer, v)
-		v.SetInnerIdx(len(newContainer) -1)
+		v.SetInnerIdx(len(newContainer) - 1)
 		if this.Type == Array {
 			v.SetIdx(v.InnerIdx())
 		}
